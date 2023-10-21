5dc92717e4b5210acaa76b9c011846028ed9d99e
ReRubis <slesarenkod@gmail.com>
2023-10-21 04:46:12 +0400

    Add more logic to the channel service
    Signed-off-by: ReRubis <slesarenkod@gmail.com>

--- yutservice/services/channeldataer.py
+++ yutservice/services/channeldataer.py
@@ -1,6 +1,6 @@
-from yutservice.utils.repository import VideoRepository
+from yutservice.utils.repository import VideoRepository, ChannelRepository
 from yutservice.yureq.ytreq2 import search_for_videos
-from yutservice.models.dbmodels import YouTubeVideo
+from yutservice.models.dbmodels import YouTubeVideo, Channel
 from yutservice.schemas import respmodel
 import yt_dlp
 
@@ -8,8 +8,12 @@ import yt_dlp
 class YouTubeChannelService():
     def __init__(self, session):
         self.session = session
+        self.channel_repository = ChannelRepository(self.session)
 
     async def get_channel_data(self, id):
+        """
+        Gets the data of a channel with specified id
+        """
         channel_url = f'https://www.youtube.com/channel/{id}'
         options = {
             'quiet': True,
@@ -23,3 +27,17 @@ class YouTubeChannelService():
             result = ydl.extract_info(channel_url, download=False)
 
         return result
+    
+    async def extract_channel_data(self, id):
+        """
+        Gets data of a youtube channel
+        Saves the data to DB
+        """
+        data = self.get_channel_data(id)
+        
+        channel_to_save = Channel()
+
+        channel_to_save.channelid = data
+        self.channel_repository.save(data)
+
+        return None
